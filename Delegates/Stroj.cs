f88cf52a968f3ffb118b051cfd488c3b5bdc2f0b
CSharpFri <CSharpFri@CLASS-2-16>
2018-01-19 20:58:00 +0100

    Add project files.

---
+++ Delegates/Stroj.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Stroj
+    {
+        private string nazevStroje;
+        private Teplomer teplomer = new Teplomer();
+
+        public Stroj(string nazevStroje)
+        {
+            teplomer.PrehratiStroje
+
+
+
+            this.nazevStroje = nazevStroje;
+        }
+        private void onPrehratiStroje(int teplota)
+        {
+            Console.WriteLine("Doslo k prehriatiu Stroja")
+            VypniStroj();
+        }
+
+        public void ZapniStroj()
+        {
+            Console.WriteLine("Vypinam {0}", nazevStroje);
+        }
+
+
+    }
+
+
+}
---
+++ Delegates/Teplomer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Delegates
+{
+    class Teplomer
+    {
+        private Timer timer; //* kvoli simulacii prehriatiu stroja
+        private Random randomizer = new Random();
+
+        public Teplomer()
+        {
+            timer = new Timer(randomizer.Next(2000, 10000));
+            timer.Start();
+            timer.Elapsed();
+
+
+        }
+
+        public delegate void PrehratiHandler(int teplota);
+        public event PrehratiHandler Prehrati;
+
+
+
+        
+
+    }
+}
